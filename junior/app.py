6cfee73d102ce68c436331aa0eec8740e6133cb9
iba200 <ibrahimathiongane2006@gmail.com>
2025-08-17 23:40:37 +0000

    truc ajouter par vscode

--- junior/app.py
+++ junior/app.py
@@ -1,8 +1,32 @@
-def somme(a, b):
+"""_summary_
+"""
+
+
+def somme(a: int, b: int):
+    """_summary_
+
+    Args:
+        a (int): _description_
+        b (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return a + b
 
-def est_pair(n):
+
+def est_pair(n: int):
+    """nombre paire"""
     return n % 2 == 0
 
-def devision(n):
+
+def devision(n: int):
+    """_summary_
+
+    Args:
+        n (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return 10 / int(n)
