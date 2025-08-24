82260863fb9048b832967af2619bdb676e61299d
iba200 <ibrahimathiongane2006@gmail.com>
2025-08-24 20:56:01 +0000

    crud ideas is add

--- mini_mvp/content-idea-hub/app/forms.py
+++ mini_mvp/content-idea-hub/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 class RegisterForm(FlaskForm):
@@ -11,3 +11,9 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class IdeaForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=100)])
+    description = TextAreaField('Description', validators=[Length(max=500)])
+    tags = StringField('Tags (comma-separated)', validators=[Length(max=200)])
+    submit = SubmitField('Save')
--- mini_mvp/content-idea-hub/app/routes.py
+++ mini_mvp/content-idea-hub/app/routes.py
@@ -3,7 +3,8 @@ from flask_login import login_user, logout_user, current_user, login_required
 from app import db, login_manager
 from app.models import User
 from app.forms import RegisterForm, LoginForm
-
+from app.models import Idea
+from app.forms import IdeaForm
 bp = Blueprint('main', __name__)
 
 
@@ -24,7 +25,7 @@ def register():
         db.session.commit()
         flash('Registered successfully!')
         return redirect(url_for('main.login'))
-    return render_template('register.html', form=form)
+    return render_template('register.html', form=form, title='Register')
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -37,7 +38,7 @@ def login():
             login_user(user)
             return redirect(url_for('main.index'))
         flash('Invalid username or password')
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form, title='Login')
 
 @bp.route('/logout')
 @login_required
@@ -48,5 +49,55 @@ def logout():
 @bp.route('/')
 @login_required
 def index():
-    return 'Welcome to Dashboard!'  # Placeholder, on ajoute ideas plus tard
+    ideas = Idea.query.filter_by(user_id=current_user.id).order_by(Idea.timestamp.desc()).all()
+    return render_template('index.html', title='Dashboard', ideas=ideas)
 
+"""
+        [route for create,update and remove ideas] 
+"""
+@bp.route('/ideas', methods=['GET'])
+@login_required
+def ideas():
+    ideas = Idea.query.filter_by(user_id=current_user.id).order_by(Idea.timestamp.desc()).all()
+    return render_template('index.html', ideas=ideas)  # Reuse index as dashboard
+
+@bp.route('/idea/new', methods=['GET', 'POST'])
+@login_required
+def new_idea():
+    form = IdeaForm()
+    if form.validate_on_submit():
+        idea = Idea(title=form.title.data, description=form.description.data, tags=form.tags.data, author=current_user)
+        db.session.add(idea)
+        db.session.commit()
+        flash('Idea added!')
+        return redirect(url_for('main.index'))
+    return render_template('idea_form.html', form=form)
+
+@bp.route('/idea/<int:id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_idea(id):
+    idea = Idea.query.get_or_404(id)
+    if idea.author != current_user:
+        flash('Not authorized')
+        return redirect(url_for('main.index'))
+    form = IdeaForm(obj=idea)
+    if form.validate_on_submit():
+        idea.title = form.title.data
+        idea.description = form.description.data
+        idea.tags = form.tags.data
+        db.session.commit()
+        flash('Idea updated!')
+        return redirect(url_for('main.index'))
+    return render_template('idea_form.html', form=form, idea=idea)
+
+@bp.route('/idea/<int:id>/delete', methods=['POST'])
+@login_required
+def delete_idea(id):
+    idea = Idea.query.get_or_404(id)
+    if idea.author != current_user:
+        flash('Not authorized')
+        return redirect(url_for('main.index'))
+    db.session.delete(idea)
+    db.session.commit()
+    flash('Idea deleted!')
+    return redirect(url_for('main.index'))
